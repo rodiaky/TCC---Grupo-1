716c0568c7265b68947bd4f8c7a90c7cb618a8e1
Rodrigo <rodrigo.mada@unesp.br>
2024-09-10 10:23:28 -0300

    commit "Rodrigo 3 10/09"

--- app/Http/Controllers/CadastroAlunoController.php
+++ app/Http/Controllers/CadastroAlunoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User; // Certifique-se de que o modelo Aluno está configurado
+use App\Models\Alunos;
 
 class CadastroAlunoController extends Controller
 {
@@ -19,20 +20,26 @@ class CadastroAlunoController extends Controller
         // Validação dos dados recebidos
         $validatedData = $request->validate([
             'nome' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
+            'email' => 'required|email',
             'senha' => 'required|string|min:3',
-            'selecionar-turma' => 'required|integer|exists:turmas,id', // Certifique-se de que você tem um modelo Turma configurado
+            'turma' => 'required|integer', // Certifique-se de que você tem um modelo Turma configurado
         ]);
 
         // Criação de um novo aluno
         $aluno = new User();
-        $aluno->nome = $validatedData['nome'];
+        $aluno->name = $validatedData['nome'];
         $aluno->email = $validatedData['email'];
-        $aluno->senha = $validatedData['password']; // Hash a senha antes de armazenar
-        $aluno->turma_id = $validatedData['selecionar-turma']; // Certifique-se de que a coluna de turma no modelo está correta
+        $aluno->password = $validatedData['senha']; // Hash a senha antes de armazenar
         $aluno->save();
 
+
+        $ultimoId = User::latest()->value('id');
+        $funcionario = new Alunos();
+        $funcionario->id_turma = $validatedData['turma']; 
+        $funcionario->id_user = $ultimoId;
+        $funcionario->save();
+
         // Redirecionar com uma mensagem de sucesso
-        return redirect()->route('cadastroAluno')->with('success', 'Aluno cadastrado com sucesso!');
+        return redirect()->route('professor.cadastroAluno.index')->with('success', 'Aluno cadastrado com sucesso!');
     }
 }
